a919001c281a323ada95233b7feaf40006a016b8
professoralex13 <alex@densem.net>
2020-05-06 15:34:19 +1200

    Setup and added basic commands

---
+++ Foodah_Bot/Foodah_Bot/Modules.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodah_Bot
+{
+    public class TestModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("yeet")]
+        [Summary("yeets")]
+        public Task YeetAsync([Remainder] [Summary("the thing to yeet")] string toYeet)
+            => ReplyAsync("Yeeted " + toYeet);
+    }
+    public class EllaModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ella")]
+        public async Task EllaAsync()
+        {
+            var embed = new EmbedBuilder();
+
+            embed.WithTitle("Heres Ella");
+
+            embed.WithImageUrl("http://randomfox.ca/images/3.jpg");
+
+            await ReplyAsync("", false, embed.Build());
+        }
+    }
+    public class JoranModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("joran")]
+        public async Task JoranAsync()
+        {
+            var embed = new EmbedBuilder();
+
+            embed.WithTitle("Heres Joran");
+
+            embed.WithImageUrl("https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iKIWgaiJUtss/v2/1000x-1.jpg");
+
+            await ReplyAsync("", false, embed.Build());
+
+        }
+    }
+}
---
+++ Foodah_Bot/Foodah_Bot/Program.cs
@@ -0,0 +1,48 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+namespace Foodah_Bot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandService _service;
+        private CommandHandler _commands;
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _service = new CommandService();
+            _commands = new CommandHandler(_client, _service);
+            _client.Log += Log;
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            var token = "NzA3NDIwNDQ4NDcxNjQ2MjE4.XrIlVw.4uU5IQkPyfqD5utL27QiNuu2IyE";
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await _commands.InstallCommandsAsync();
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
